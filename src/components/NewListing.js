5b0c929317cb5fad597a83dae9ceaec496270617
marisasaunders <marisaksaunders@gmail.com>
2022-10-14 00:07:59 -0400

    create handleSubmit function

--- src/components/NewListing.js
+++ src/components/NewListing.js
@@ -18,10 +18,30 @@ function NewListing({ users, addNewListing}) {
     body: JSON.stringify(newList),
   };
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  //   fetch
-  // }
+    fetch("http://localhost:9292/listings", configObj)
+      .then((r) => r.json())
+      .then((listing) => {
+        addNewListing(listing);
+      });
+  };
+
+  return (
+    <div>
+      <h3>Create a new listing:</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          value={title}
+          
+        />
+      </form>
+    </div>
+  )
 
 }
