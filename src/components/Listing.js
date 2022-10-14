fee8e006ad3380168ec6505e75619db5a3f899ec
marisasaunders <marisaksaunders@gmail.com>
2022-10-14 00:50:55 -0400

    copy bootstsrap card to listing

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,8 +1,47 @@
 import React, { useState } from "react";
-import EditListing from "./EditListing";
+import { Button, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Listing({ listing, onUpdateListing }) {
   const { title, body } = listing;
+
+  return (
+    <Card>
+      <div>
+        <div className="image">
+          <img 
+            src={image} 
+            alt={name} 
+            style={styles.img} 
+          />
+        </div>
+        <h2  
+          style={styles.center}
+          >{name}
+        </h2>
+        <div className="content">
+          <div 
+            className="header" 
+            style={styles.center}
+            >{artist}
+          </div>
+        </div>
+        <div className="extra content">
+          <Button 
+            onClick={handleLikeClick} 
+            style={styles.likeButton}
+            >💗 {likes}
+          </Button>
+          <Button 
+            style={styles.trashButton}
+            onClick={() => handleDelete(music.id)}
+            >🗑
+          </Button>
+          <Link to={`/${id}/edit`}><button style={styles.trashButton}>EDIT</button></Link>
+        </div>
+      </div>
+    </Card>
+  )
 
 }
 
