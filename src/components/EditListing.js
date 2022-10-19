960b0c551bd383a9a478c657cde1aa565d4d30d3
marisasaunders <marisaksaunders@gmail.com>
2022-10-18 22:44:08 -0400

    edit function is working

--- src/components/EditListing.js
+++ src/components/EditListing.js
@@ -3,8 +3,20 @@ import React, { useState } from 'react';
 function EditListing({ listing, handleUpdateListing}) {
   const { id, title, body, cat_id } = listing;
 
-  const [updatedTitle, setUpdatedTitle] = useState(title);
-  const [updatedBody, setUpdatedBody] = useState(body);
+  // const defaultFormData = {
+  //   title: title,
+  //   body: body
+  // }
+
+  const [formData, setFormData] = useState({
+    title: title,
+    body: body
+  });
+
+  // const [formData, setFormData] = useState(defaultFormData)
+
+  // const [updatedTitle, setUpdatedTitle] = useState(title);
+  // const [updatedBody, setUpdatedBody] = useState(body);
 
   function handleEditListing(e) {
     e.preventDefault();
@@ -14,31 +26,49 @@ function EditListing({ listing, handleUpdateListing}) {
     headers: { 
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ title: updatedTitle, body: updatedBody }),
+    body: JSON.stringify(formData),
    })
     .then ((response) => response.json())
     .then((updatedListing) => {
-      handleEditListing(updatedListing)
+      handleUpdateListing(updatedListing)
     });
   }
+
+  function handleChange(e){
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  // function handleEditListing(e) {
+  //   e.preventDefault();
+
+  //   fetch(`http://localhost:9393/listings/${id}`, {
+  //   method: "PATCH",
+  //   headers: { 
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({ title: updatedTitle, body: updatedBody }),
+  //  })
+  //   .then ((response) => response.json())
+  //   .then((updatedListing) => {
+  //     handleEditListing(updatedListing)
+  //   });
+  // }
   
   return (
     <form onSubmit={handleEditListing}>
       <label htmlFor="title">Title:</label>
       <input
-        id="title"
         type="text"
         name="title"
-        value="updatedTitle"
-        onChange={(e) => setUpdatedTitle(e.target.value)}
+        value={formData.title}
+        onChange={handleChange}
       />
       <label htmlFor="body ">Body:</label>
       <input
-        id="body "
         type="text"
         name="body"
-        value="updatedBody"
-        onChange={(e) => setUpdatedBody(e.target.value)}
+        value={formData.body}
+        onChange={handleChange}
       />
       <input type="submit" value="Save" />
     </form>
